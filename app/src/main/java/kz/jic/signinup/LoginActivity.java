e68448d2a828bae84705767dc2ad75cad52f3729
Allaniy Sultan <allaniyazov.sultan@mail.ru>
2020-09-07 18:16:12 +0600

    fifth commit

--- app/src/main/java/kz/jic/signinup/LoginActivity.java
+++ app/src/main/java/kz/jic/signinup/LoginActivity.java
@@ -2,13 +2,26 @@ package kz.jic.signinup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        img = findViewById(R.id.goBackIcon);
+    }
+
+    public void goback(View view) {
+        Intent ic = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(ic);
     }
 }
