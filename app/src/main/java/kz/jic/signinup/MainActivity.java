e56649c31a77ac4474b4f7a4d851ea72e14fa147
Allaniy Sultan <allaniyazov.sultan@mail.ru>
2020-09-07 16:34:17 +0600

    second commit

--- app/src/main/java/kz/jic/signinup/MainActivity.java
+++ app/src/main/java/kz/jic/signinup/MainActivity.java
@@ -2,13 +2,25 @@ package kz.jic.signinup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.joinus);
+    }
+
+    public void goToNext(View view) {
+        Intent dsp = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(dsp);
     }
 }
